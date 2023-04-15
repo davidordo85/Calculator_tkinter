9beba8aed7f8e57ecd2cbd944995bce4e195dd28
David Ordoñez <davidordonezescacena@hotmail.es>
2023-04-15 19:48:35 +0200

    fixes and additions to enter roman calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,9 @@
 from tkinter import *
 from tkinter import ttk
+from romans import RomanNumber
 
-dButtons = [
+
+normal_buttons = [
     {
         "text": "1",
         "col": 0,
@@ -95,77 +97,200 @@ dButtons = [
     }
 ]
 
+roman_buttons = [
+    {
+        "text": "=",
+        "col": 0,
+        "row": 5,
+        "W": 4
+    },
+    {
+        "text": "I",
+        "col": 0,
+        "row": 4,
+    },
+    {
+        "text": "V",
+        "col": 1,
+        "row": 4,
+    },
+    {
+        "text": "X",
+        "col": 0,
+        "row": 3,
+    },
+    {
+        "text": "L",
+        "col": 1,
+        "row": 3,
+    },
+    {
+        "text": "C",
+        "col": 0,
+        "row": 2,
+    },
+    {
+        "text": "D",
+        "col": 1,
+        "row": 2,
+    },
+    {
+        "text": "M",
+        "col": 2,
+        "row": 2,
+        "H": 3
+    },
+    {
+        "text": "AC",
+        "col": 1,
+        "row": 1,
+        "W": 2
+    },
+    {
+        "text": "÷",
+        "col": 3,
+        "row": 1,
+    },
+    {
+        "text": "x",
+        "col": 3,
+        "row": 2,
+    },
+    {
+        "text": "-",
+        "col": 3,
+        "row": 3,
+    },
+    {
+        "text": "+",
+        "col": 3,
+        "row": 4,
+    }
+]
+
+def pinta(valor):
+    print(valor)
+    return valor
+
 class Controller(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent, width=272, height=300)
-        self.option_1 = 0
-        self.option_2 = 0
-        self.operation = ''
-        self.displayValue = '0'
+        self.reset()
+        self.status = "N"
 
         self.display = Display(self)
-        self.display.grid(column=0, row=0, columnspan=4)
+        self.display.grid(column=0, row=0)
 
-        for properties in dButtons:
-            btn = CalcButton(self, properties['text'], self.set_operation, properties.get("W", 1), properties.get("H", 1))
-            btn.grid(column=properties['col'], row=properties['row'], columnspan=properties.get("W", 1), rowspan=properties.get("H", 1))
+        self.keyboard = Keyboard(self, self.set_operation, self.status)
+        self.keyboard.grid(column=0, row=1)
+
+        self.selector = Selector(self.keyboard, self.change_status, self.status)
+        self.selector.grid(column=0, row=1)
+
+    def reset(self):
+        self.op1 = None
+        self.op2 = None
+        self.operation = ''
+        self.displayValue = '0'
+        self.just_pressed_sign = False
 
     def to_float(self, valor):
         return float(valor.replace(',', '.'))
 
+    def to_str(self, valor):
+        return str(valor).replace('.', ',')
+
     def calculate(self):
         if self.operation == '+':
-            return self.option_1 + self.option_2
+            return self.op1 + self.op2
         elif self.operation == '-':
-            return self.option_1 - self.option_2
+            return self.op1 - self.op2
         elif self.operation == 'x':
-            return self.option_1 * self.option_2
+            return self.op1 * self.op2
         elif self.operation == '÷':
-            return self.option_1 / self.option_2
+            return self.op1 / self.op2
 
-        return self.option_2
+        return self.op2
 
     def set_operation(self, something):
-        if something.isdigit():
-            if self.displayValue == "0":
+
+        if self.status == "R":
+            self.set_operation_R(something)
+        else:
+            self.set_operation_N(something)
+            
+        self.display.paint(self.displayValue)
+
+    def set_operation_R(self, something):
+        print("Developing")
+
+    def set_operation_N(self, something):
+        if something.isdigit(): # Check if something is in (IVXLCDM) and check if the roman numeral is formatted correctly. Change displayValue
+            if self.displayValue == "0" or self.just_pressed_sign:
+                self.op1 = self.to_float(self.displayValue)
+                self.op2 = None
                 self.displayValue = something
             else:
                 self.displayValue += str(something)
         
-        if something == 'C':
-            self.displayValue = '0'
+        if something == 'C': # and it is Arabic if it is not 'AC'
+            self.reset()
 
-        if something == '+/-' and self.displayValue != '0':
+        if something == '+/-' and self.displayValue != '0': # Does not work in roman
             if self.displayValue[0] == '-':
                 self.displayValue = self.displayValue[1:]
             else:
                 self.displayValue = '-' + self.displayValue
 
-        if something == ',' and ',' not in self.displayValue:
+        if something == ',' and ',' not in self.displayValue: # Does not work in roman
             self.displayValue += str(something)
 
-        if something == '+' or something == '-' or something == 'x' or something == '÷':
-            self.option_1 = self.to_float(self.displayValue)
-            self.operation = something
-            self.displayValue = '0'
+        if something == '+' or something == '-' or something =='x' or something =='÷':
+            if not self.op1:
+                self.op1 = self.to_float(self.displayValue) # Pass to RomanNumber if status is R
+                self.operation = something
+            elif not self.op2:
+                self.op2 = self.to_float(self.displayValue) # Pass to RomanNumber if status is R
+                res = self.calculate()
+                self.displayValue = self.to_str(res)
+                self.operation = something
+            else: 
+                self.op1 = self.to_float(self.displayValue) # Pass to RomanNumber if status is R
+                self.op2 = None
+                self.operation = something
+            self.just_pressed_sign = True
+        else:
+            self.just_pressed_sign = False
 
         if something == '=':
-            self.option_2 = self.to_float(self.displayValue)
-            res = self.calculate()
-            self.displayValue = str(res)
+            if self.op1 and not self.op2:
+                self.op2 = self.to_float(self.displayValue)
+                res = self.calculate()
+                self.displayValue = self.to_str(res)
+
+            elif self.op1 and self.op2:
+                self.op1 = self.to_float(self.displayValue)
+                res = self.calculate()
+                self.displayValue = self.to_str(res)
 
 
-        self.display.paint(self.displayValue)
+
+    def change_status(self, status):
+        self.status = status
+        self.keyboard.status = status
+        self.reset()
+        
 
 class Display(ttk.Frame):
     value = "0"
+    
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent, width=272, height=50)
         self.pack_propagate(0)
 
         s = ttk.Style()
         s.theme_use('alt')
-        s.configure('my.TLabel', font='Helvetica 30', background='black', foreground='white')
+        s.configure('my.TLabel', font='Helvetica 36', background='black', foreground='white')
 
         self.lbl = ttk.Label(self, text=self.value, anchor=E, style='my.TLabel')
         self.lbl.pack(side=TOP, fill=BOTH, expand=True)
@@ -174,11 +299,80 @@ class Display(ttk.Frame):
         self.value = something
         self.lbl.config(text=something)
 
+        
+class Selector(ttk.Frame):
+    def __init__(self, parent, command, status="N"):
+        ttk.Frame.__init__(self, parent, width=68, height=50)
+        self.status = status
+        self.__value = StringVar()
+        self.__value.set(self.status)
+        self.command = command
 
-class Selector(ttk.Radiobutton):
-    pass
+        radioB1 = ttk.Radiobutton(self, text="N", value="N", name="rbtn_normal", variable=self.__value, command=self.__click)
+        radioB1.place(x=0, y=5)
+        radioB2 = ttk.Radiobutton(self, text="R", value="R", name="rbtn_romano", variable=self.__value, command=self.__click)
+        radioB2.place(x=0, y=30)
 
-class CalcButton(ttk.Button):
+    def __click(self):
+        self.status = self.__value.get()
+        self.command(self.status)
+
+
+
+class Keyboard(ttk.Frame):
+    def __init__(self, parent, command, status="N"):
+        ttk.Frame.__init__(self, parent, height=250, width=272)
+        self.__status = status
+        self.buttonListRomans = []
+        self.buttonListNormal = []
+        self.command = command
+        
+        if self.__status == "N":
+            self.normalPaint()
+        else:
+            self.romanPaint()
+
+    @property
+    def status(self):
+        return self.__status
+
+    @status.setter
+    def status(self, valor):
+        self.__status = valor
+        if valor == 'N':
+            self.normalPaint()
+        else:
+            self.normalRoman()
+
+    def normalPaint(self):
+        if len(self.buttonListNormal) == 0:
+            for properties in normal_buttons:
+                btn = CalcButton(self, properties['text'], self.command, properties.get("W", 1), properties.get("H", 1))
+                self.buttonListNormal.append((btn, properties))
+                btn.grid(column=properties['col'], row=properties['row'], columnspan=properties.get("W", 1), rowspan=properties.get("H", 1))
+        else:
+            for btn, properties in self.buttonListNormal: 
+                 btn.grid(column=properties['col'], row=properties['row'], columnspan=properties.get("W", 1), rowspan=properties.get("H", 1))
+
+        for delete, properties in self.buttonListRomans:
+            delete.grid_forget()
+
+    def normalRoman(self):
+        if len(self.buttonListRomans) == 0:
+            for properties in roman_buttons:
+                btn = CalcButton(self, properties['text'], self.command, properties.get("W", 1), properties.get("H", 1))
+                self.buttonListRomans.append((btn, properties))
+                btn.grid(column=properties['col'], row=properties['row'], columnspan=properties.get("W", 1), rowspan=properties.get("H", 1))
+        else:
+            for btn, properties in self.buttonListRomans:
+                btn.grid(column=properties['col'], row=properties['row'], columnspan=properties.get("W", 1), rowspan=properties.get("H", 1))
+
+        for delete, properties in self.buttonListNormal:
+            delete.grid_forget()
+
+
+
+class CalcButton(ttk.Frame):
     def __init__(self, parent, value, command, width=1, height=1):
         ttk.Frame.__init__(self, parent, width=68*width, height=50*height)
         self.pack_propagate(0)
---
+++ romans.py
@@ -0,0 +1,123 @@
+class RomanNumber():
+    __symbols = {'M': 1000,
+          'CM': 900,
+          'D': 500,
+          'CD': 400,
+          'C': 100,
+          'XC': 400,
+          'L': 50,
+          'XL': 40,
+          'X': 10,
+          'IX': 9,
+          'V': 5,
+          'IV': 4,
+          'I': 1,
+    }
+
+    def __init__(self, value):
+        if isinstance(value, str) and value.isdigit() == False:
+            self.valor = self.roman_number_to_integer(value)
+            if self.valor == 'Format error':
+                self.rValor = self.valor
+            else:
+                self.rValor = str(self.valor)
+        else:
+            self.valor = int(value)
+            self.rValor = self.integer_to_roman_number()
+            if self.rValor == 'Overflow':
+                self.valor = self.rValor
+
+
+    def roman_number_to_integer(self, roman_number):
+        if roman_number == '':
+            return 'Format error'
+
+        integer = 0
+        repeated_number = 1
+        previous_letter = ''
+        was_subtraction = False
+        for letter in roman_number:
+
+            if letter in self.__symbols:
+                if previous_letter == '' or self.__symbols[previous_letter] >= self.__symbols[letter]:
+                    integer += self.__symbols[letter]
+                    was_subtraction = False
+                else:
+                    if previous_letter + letter in self.__symbols.keys() and repeated_number < 2 and not was_subtraction:
+                        integer = integer - self.__symbols[previous_letter] * 2 + self.__symbols[letter]
+                        was_subtraction = True
+                    else:
+                        return 'Format error'
+            else:
+                return 'Format error'
+
+            if letter == previous_letter and repeated_number == 3:
+                return 'Format error'
+            elif letter == previous_letter:
+                repeated_number += 1
+            else:
+                repeated_number = 1
+            
+            previous_letter = letter
+
+        return integer
+
+    def integer_to_roman_number(self):
+        if self.valor > 3999 or self.valor < 1:
+            return 'Overflow'
+        components = self.__discompose(self.valor)
+        res = ''        
+        for value in components:
+            while value > 0:
+                k, v = self.__find_less_than_or_equal_value(value)
+                value -= v
+                res += k
+        return res
+
+    def __find_less_than_or_equal_value(self, v):
+        for key, value in self.__symbols.items():
+            if value <= v:
+                return key, value
+
+    def __discompose(self, number):
+        res = []
+        for order in range(3, 0, -1):
+            rest = number % 10 ** order
+            res.append(number - rest)
+            number = rest
+        res.append(number)
+        return res
+
+    def __str__(self):
+        return self.rValor
+
+    def __repr__(self):
+        return self.rValor
+
+
+    def __add__(self, other):
+        #       I,    III
+        if isinstance(other, int):
+            addiction = self.valor + other
+        else:
+            addiction = self.valor + other.valor
+        result = RomanNumber(addiction)
+        return result
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        if isinstance(other, int):
+            subtraction = self.valor + other
+        else:
+            subtraction = self.valor + other.valor
+        result = RomanNumber(subtraction)
+        return result
+
+    def __rsub__(self, other):
+        return self.sub(other)
+
+    def __eq__(self, other):
+        return self.valor == other.valor
+     
